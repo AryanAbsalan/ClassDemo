baeb76e52f63561b324b287ca3b29f05f4beaaa8
Aryan Absalan <56235353+AryanAbsalan@users.noreply.github.com>
2020-10-05 22:29:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Example of defining a class in C# , setting its properties and calling its method/s.");
+
+            // defining the first object of student class as tim
+            Student tim = new Student();
+            // setting the poblic attributes for the first object
+            tim.FirstName = "Tim";
+            tim.LastName = "Timmabc";
+
+
+            // defining the second object of student class as jim
+            Student jim = new Student();
+            // setting the poblic attributes for the second object
+            jim.FirstName = "Jim";
+            jim.LastName = "Jimmxyz";
+
+            //Calling the Display method for both objects
+            tim.Display();
+            jim.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    public class Student
+    {
+
+        //Public properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Default constructor.
+        public Student()
+        {
+
+        }
+
+        //Public method
+        public void Display()
+        {
+            Console.WriteLine("\t Student first name:  " + this.FirstName);
+            Console.WriteLine("\t Student last name:  " + this.LastName);
+        }
+    }
+}
